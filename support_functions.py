1431f762e119125f6db76aa31605339a8388b61a
nadya sitdikova <nchebaeva@yandex.ru>
2022-10-02 20:02:23 +0200

    user_id added

--- support_functions.py
+++ support_functions.py
@@ -1,9 +1,15 @@
 from db import select_from_table
 
 
-def cur_user_id_finder(login):
-    condition = "user = '{}'".format(login)
-    user_id = select_from_table('users', 'userID', condition)
+def cur_user_id_finder(user_id, login):
+    if user_id:
+        condition = "userID = '{}'".format(user_id)
+        user_id = select_from_table('users', 'userID', condition)
+        print('if')
+    elif login:
+        condition = "user = '{}'".format(login)
+        user_id = select_from_table('users', 'userID', condition)
+        print('elif')
     return user_id
 
 
@@ -16,4 +22,3 @@ def is_admin(login):
     condition = "login = '{}'".format(login)
     is_true = len(select_from_table('admins', 'login', condition)) == 1
     return is_true
-
